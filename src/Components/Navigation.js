11015a929167f54168621cf4e8eb2a310001dcee
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2022-05-25 09:06:32 -0300

    part 2

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,5 @@
-import { styled } from '@material-ui/core';
 import React from 'react'
+import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
 import avatar from '../img/avatar.jpg';
 
@@ -7,19 +7,48 @@ function Navigation() {
   return (
     <NavigationStyled>
         <div className="avatar">
-            <img scr={avatar} alt=""/>
+            <img className="img" scr={avatar} alt=""/>
         </div>
         <ul className="nav-items">
             <li className="nav-item">
                 <NavLink to="/home" activeClassName="active-class">Home</NavLink>
             </li>
         </ul>
+        <ul className="nav-items">
+            <li className="nav-item">
+                <NavLink to="/home" activeClassName="active-class">About</NavLink>
+            </li>
+        </ul>
+        <ul className="nav-items">
+            <li className="nav-item">
+                <NavLink to="/home" activeClassName="active-class">Contact</NavLink>
+            </li>
+        </ul>
+        <footer className="footer">
+          <p>@2022</p>
+        </footer>
     </NavigationStyled>
   )
 }
 
 const NavigationStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  aling-items: center;
+  height: 100%;
+  width: 100%;
 
-
+  .avatar{
+    width: 100%;
+    border-bottom: 1px solid var(--border-color);
+    text-align: center;
+    padding: 2rem 0;
+    img{
+      width: 100%;
+      border-radius: 50%;
+      border: 8px solid var (--border-color);
+    }
+  }
 `;
 export default Navigation;
